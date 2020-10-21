b661b06e07a3dbcf6d7dde2df85122bab3655509
mmullis07 <eddiemull311@gmail.com>
2020-10-21 11:24:25 -0700

    Finished Create Functionality. Accidentally started update functionality

--- src/pages/UserEdit.js
+++ src/pages/UserEdit.js
@@ -1,10 +1,81 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Redirect } from 'react-router-dom'
+
 
 export default class UserEdit extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            form: {
+                name: "",
+                age: "",
+                address: "",
+                email: ""
+            },
+            success: false
+        }
+    }
+
+    handleChange = (e) => {
+        let { form } = this.state
+        form[e.target.name] = e.target.value
+        this.setState({ form: form })
+    }
+
+    handleSubmit = () => {
+        this.props.editUser(this.state.form)
+        this.setState({ success: true })
+    }
     render() {
         return (
             <React.Fragment>
                 <h3>Edit Account</h3>
+                <Form>
+                    <FormGroup>
+                        <Label for="name">Update your Name</Label>
+                        <Input
+                            type="text"
+                            name="name"
+                            onChange={this.handleChange}
+                            value={this.state.form.name}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="age">Update your Age</Label>
+                        <Input
+                            type="number"
+                            name="age"
+                            onChange={this.handleChange}
+                            value={this.state.form.age}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="address">Update your address</Label>
+                        <Input
+                            type="text"
+                            name="address"
+                            onChange={this.handleChange}
+                            value={this.state.form.address}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Update your email</Label>
+                        <Input
+                            type="email"
+                            name="email"
+                            onChange={this.handleChange}
+                            value={this.state.form.email}
+                        />
+                    </FormGroup>
+                    <Button
+                        name="submit"
+                        onClick={this.handleSubmit}
+                    >
+                        Update Account
+          </Button>
+                </Form>
+                { this.state.success && <Redirect to="/petindex" />}
             </React.Fragment>
         )
     }
--- src/pages/UserNew.js
+++ src/pages/UserNew.js
@@ -1,10 +1,82 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Redirect } from 'react-router-dom'
+
 
 export default class UserNew extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            form: {
+                name: "",
+                age: "",
+                address: "",
+                email: ""
+            },
+            success: false
+        }
+    }
+
+    handleChange = (e) => {
+        let { form } = this.state
+        form[e.target.name] = e.target.value
+        this.setState({ form: form })
+    }
+
+    handleSubmit = () => {
+        this.props.createNewUser(this.state.form)
+        this.setState({ success: true })
+    }
+
     render() {
         return (
             <React.Fragment>
                 <h3>New User</h3>
+                <Form>
+                    <FormGroup>
+                        <Label for="name">Enter your Name</Label>
+                        <Input
+                            type="text"
+                            name="name"
+                            onChange={this.handleChange}
+                            value={this.state.form.name}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="age">Enter your Age</Label>
+                        <Input
+                            type="number"
+                            name="age"
+                            onChange={this.handleChange}
+                            value={this.state.form.age}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="address">Enter your address</Label>
+                        <Input
+                            type="text"
+                            name="address"
+                            onChange={this.handleChange}
+                            value={this.state.form.address}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Enter your email</Label>
+                        <Input
+                            type="email"
+                            name="email"
+                            onChange={this.handleChange}
+                            value={this.state.form.email}
+                        />
+                    </FormGroup>
+                    <Button
+                        name="submit"
+                        onClick={this.handleSubmit}
+                    >
+                        Create New User Account
+          </Button>
+                </Form>
+                { this.state.success && <Redirect to="/petindex" />}
             </React.Fragment>
         )
     }
